ca9ac0a6f1153afcab3a7e2f65abf1ed6b8bf696
sachinthakurst <sachin.t@techsophy.com>
2020-10-31 09:41:36 +0530

    simple tip calculator

---
+++ 18-tip-calculator/script.js
@@ -0,0 +1,19 @@
+const inputElt = document.querySelector("input");
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+
+let cost;
+let totalTip;
+
+function setInputValue(e) {
+  cost = e.target.value;
+}
+
+function calculateTip() {
+  totalTip = Math.floor(cost * 0.25);
+  output.textContent = `You Should Tip ₹${totalTip} on ₹${cost}`;
+  output.style.visibility = "unset";
+}
+
+inputElt.addEventListener("change", setInputValue);
+button.addEventListener("click", calculateTip);
