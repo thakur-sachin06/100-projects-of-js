fa6d2102714957412358febe3d45fc751e41247c
thakursachin1004 <sachinst000@gmail.com>
2022-05-12 18:22:38 +0530

    created 10X10 board

--- 24-snake-ladder-game/script.js
+++ 24-snake-ladder-game/script.js
@@ -1,14 +1,17 @@
 const container = document.getElementById("grid-container");
 
-let num = 100;
+let num = 1;
 
 const createBoard = () => {
-  for (let i = 10; i >= 1; i--) {
+  for (let i = 1; i <= 10; i++) {
+    const boxContainer = document.createElement("div");
+    container.appendChild(boxContainer);
     for (let j = num; j <= i * 10; j++) {
-      const box = document.createElement("span");
+      const box = document.createElement("div");
+      box.classList.add("box");
       box.innerHTML = num;
-      container.appendChild(box);
-      num--;
+      boxContainer.appendChild(box);
+      num++;
     }
   }
 };
