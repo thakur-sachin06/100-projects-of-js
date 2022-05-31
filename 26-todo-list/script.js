2a48e671f0e4f24d02522bfd2f1c2983f8af8023
thakursachin1004 <sachinst000@gmail.com>
2022-06-01 00:50:39 +0530

    completed toggle tasks functionality

--- 26-todo-list/script.js
+++ 26-todo-list/script.js
@@ -2,31 +2,64 @@ let tasks = [
   {
     id: 1,
     title: "Complete Angular Course",
-    isCompleted: false,
+    status: "TODO",
   },
   {
     id: 2,
     title: "Complete DSA Course",
-    isCompleted: false,
+    status: "INPROGRESS",
   },
   {
     id: 3,
     title: "Complete 100-days-of-js",
-    isCompleted: false,
+    status: "COMPLETED",
   },
 ];
 
-const inProgressSection = document.getElementById("inprogress-tasks");
+const todoTasks = tasks.filter((task) => task.status === "TODO");
+const inprogressTasks = tasks.filter((task) => task.status === "INPROGRESS");
+const completedTasks = tasks.filter((task) => task.status === "COMPLETED");
 
-function createTasksList() {
-  const inprogressTasks = tasks.filter((task) => !task.isCompleted);
-  const fragment = document.createDocumentFragment();
-  inprogressTasks.forEach((task) => {
-    const input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    fragment.appendChild(input);
-  });
-  inProgressSection.appendChild(fragment);
+const toDoSection = document.getElementById("to-do-section");
+const inProgressSection = document.getElementById("inprogress-section");
+const completedSection = document.getElementById("completed-section");
+
+function toggleTasksInput(e, task) {
+  const isChecked = e.target.checked;
+  const toggledTask = document.getElementById(`task-${task.id}`);
+  if (isChecked) {
+    completedSection.appendChild(toggledTask);
+  } else {
+    toDoSection.appendChild(toggledTask);
+  }
 }
 
-createTasksList();
+function createTasksList(tasks, section) {
+  const fragment = document.createDocumentFragment();
+  tasks.forEach((task) => {
+    const container = document.createElement("div");
+    container.setAttribute("id", `task-${task.id}`);
+    container.classList.add("tasks");
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("name", `task-${task.id}`);
+    input.setAttribute("value", task.title);
+    input.setAttribute("id", `task-${task.id}`);
+    if (task.status === "COMPLETED") {
+      input.checked = true;
+    }
+    input.addEventListener("change", (e) => toggleTasksInput(e, task));
+    const label = document.createElement("label");
+    label.setAttribute("for", `task-${task.id}`);
+    label.innerText = task.title;
+    label.style.marginLeft = "10px";
+    container.appendChild(input);
+    container.appendChild(label);
+    fragment.appendChild(container);
+  });
+  section.appendChild(fragment);
+}
+
+createTasksList(todoTasks, toDoSection);
+createTasksList(inprogressTasks, inProgressSection);
+createTasksList(completedTasks, completedSection);
