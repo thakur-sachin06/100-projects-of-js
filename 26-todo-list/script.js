d74c613b9ddd08cdb3ee3260fce75cb62aecc92e
thakursachin1004 <sachinst000@gmail.com>
2022-06-02 01:06:14 +0530

    completed adding new task

--- 26-todo-list/script.js
+++ 26-todo-list/script.js
@@ -1,3 +1,7 @@
+const TODO = "TODO";
+const INPROGRESS = "INPROGRESS";
+const COMPLETED = "COMPLETED";
+
 let tasks = [
   {
     id: 1,
@@ -16,57 +20,127 @@ let tasks = [
   },
 ];
 
-const todoTasks = tasks.filter((task) => task.status === "TODO");
-const inprogressTasks = tasks.filter((task) => task.status === "INPROGRESS");
-const completedTasks = tasks.filter((task) => task.status === "COMPLETED");
+let newTaskName = "";
+let newTaskStatus = TODO;
+
+const todoTasks = tasks.filter((task) => task.status === TODO);
+const inprogressTasks = tasks.filter((task) => task.status === INPROGRESS);
+const completedTasks = tasks.filter((task) => task.status === COMPLETED);
+
+const mainAddTaskBtn = document.getElementById("main-btn");
+
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modal-add-btn");
+const modalInput = document.getElementById("modal-input");
+const modalSelect = document.getElementById("modal-select");
 
 const toDoSection = document.getElementById("task-container");
 const inProgressSection = document.getElementById("inprogress-section");
 const completedSection = document.getElementById("completed-section");
+
+function getBackgroundColor(task) {
+  if (task.status === TODO) {
+    return "#f1dada";
+  } else if (task.status === INPROGRESS) {
+    return "rgb(233, 243, 255)";
+  } else {
+    return "rgb(233, 243, 234)";
+  }
+}
 
 function toggleTasksInput(e) {
   const isChecked = e.target.checked;
   const toggledTask = document.getElementById(e.target.getAttribute("id"));
   if (isChecked) {
     completedSection.appendChild(toggledTask);
+    toggledTask.style.background = "rgb(233, 243, 234)";
   } else {
     toDoSection.appendChild(toggledTask);
+    toggledTask.style.background = "#f1dada";
   }
+}
+
+function createTask(task) {
+  const container = document.createElement("div");
+  container.setAttribute("id", `task-${task.id}`);
+  container.classList.add("tasks");
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("name", `task-${task.id}`);
+  input.setAttribute("value", task.title);
+  input.setAttribute("id", `task-${task.id}`);
+  input.style.cursor = "pointer";
+  input.style.width = "20px";
+  input.style.height = "20px";
+
+  if (task.status === "COMPLETED") {
+    input.checked = true;
+  }
+  container.style.background = getBackgroundColor(task);
+  const label = document.createElement("label");
+  label.setAttribute("for", `task-${task.id}`);
+  label.innerText = task.title;
+  label.style.marginLeft = "10px";
+  container.appendChild(input);
+  container.appendChild(label);
+  return container;
 }
 
 function createTasksList(tasks, section) {
   const fragment = document.createDocumentFragment();
   tasks.forEach((task) => {
-    const container = document.createElement("div");
-    container.setAttribute("id", `task-${task.id}`);
-    container.classList.add("tasks");
-    const input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("name", `task-${task.id}`);
-    input.setAttribute("value", task.title);
-    input.setAttribute("id", `task-${task.id}`);
-    input.style.cursor = "pointer";
-    input.style.width = "20px";
-    input.style.height = "20px";
-
-    if (task.status === "COMPLETED") {
-      input.checked = true;
-    }
-    const label = document.createElement("label");
-    label.setAttribute("for", `task-${task.id}`);
-    label.innerText = task.title;
-    label.style.marginLeft = "10px";
-    container.appendChild(input);
-    container.appendChild(label);
+    const container = createTask(task, section);
     fragment.appendChild(container);
   });
   section.appendChild(fragment);
 }
 
-toDoSection.addEventListener("change", toggleTasksInput);
-inProgressSection.addEventListener("change", toggleTasksInput);
-completedSection.addEventListener("change", toggleTasksInput);
+function createTasks() {
+  createTasksList(todoTasks, toDoSection);
+  createTasksList(inprogressTasks, inProgressSection);
+  createTasksList(completedTasks, completedSection);
+  toDoSection.addEventListener("change", toggleTasksInput);
+  inProgressSection.addEventListener("change", toggleTasksInput);
+  completedSection.addEventListener("change", toggleTasksInput);
+}
 
-createTasksList(todoTasks, toDoSection);
-createTasksList(inprogressTasks, inProgressSection);
-createTasksList(completedTasks, completedSection);
+// Add task modal
+
+function addNewTask() {
+  if (!newTaskName.length) {
+    return;
+  }
+  const newTask = {
+    id: tasks.length + 1,
+    title: newTaskName,
+    status: newTaskStatus,
+  };
+  const container = createTask(newTask);
+  if (newTask.status === TODO) {
+    toDoSection.appendChild(container);
+  } else if (newTask.status === INPROGRESS) {
+    inProgressSection.appendChild(container);
+  } else {
+    completedSection.appendChild(container);
+  }
+  tasks.push(newTask);
+  newTaskName = "";
+  newTaskStatus = TODO;
+  modalInput.value = "";
+  modalSelect.value = TODO;
+  modal.style.display = "none";
+}
+
+modalBtn.addEventListener("click", addNewTask);
+modalInput.addEventListener("change", (e) => {
+  newTaskName = e.target.value;
+});
+modalSelect.addEventListener("change", (e) => {
+  newTaskStatus = e.target.value;
+});
+
+mainAddTaskBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+createTasks();
