e356c88946175c9fb1abe24761d551a521904a44
thakursachin1004 <sachinst000@gmail.com>
2022-06-10 23:21:16 +0530

    startd star rating component

---
+++ 28-star-component/script.js
@@ -0,0 +1,9 @@
+const comments = [
+  {
+    comment: "this song is realle good",
+    replies: [
+      { comment: "this is reply to comment 1", replies: [] },
+      { comment: "this is reply to comment 2", replies: [] },
+    ],
+  },
+];
