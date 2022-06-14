c730f1f745426c63cd9221445e358fa3be5c4531
thakursachin1004 <sachinst000@gmail.com>
2022-06-14 23:58:36 +0530

    array flat with nesting level

--- JS_Polyfills/array-methods/flat.js
+++ JS_Polyfills/array-methods/flat.js
@@ -1,12 +1,17 @@
-function flat(arr) {
+function flat(arr, level) {
   let flatArr = [];
+  let count = 0;
 
   function child(arr) {
     arr.map((elt) => {
-      if (typeof elt === "object") {
+      if (typeof elt === "object" && count < level) {
+        count++;
         child(elt);
+      } else if (typeof elt !== "object") {
+        flatArr.push(elt);
       } else {
         flatArr.push(elt);
+        count = 0;
       }
     });
   }
@@ -15,4 +20,4 @@ function flat(arr) {
   console.log(flatArr);
 }
 
-flat([1, [2, [10, 20, 40, 50], 900], 3, 4], 1);
+flat([1, [2, [10, 20, 40, 50], [900, 1000, 2000]], 3, 4], 2);
