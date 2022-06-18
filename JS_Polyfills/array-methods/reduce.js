cde083b4176e0236c41836d7da6343fdf9b28d89
thakursachin1004 <sachinst000@gmail.com>
2022-06-19 00:45:14 +0530

    reduce polyfill

--- JS_Polyfills/array-methods/reduce.js
+++ JS_Polyfills/array-methods/reduce.js
@@ -1,2 +1,25 @@
 // started reduce polyfill
-function reducePolyfill() {}
+Array.prototype.reducePolyfill = function (callback, startingValue) {
+  let i = 0,
+    acc = startingValue;
+  if (acc === undefined) {
+    acc = this[0];
+    i = 1;
+  }
+
+  for (; i < this.length; i++) {
+    acc = callback(acc, this[i]);
+  }
+
+  return acc;
+};
+
+const result = [1, 2, 3, 4].reducePolyfill(function (a, b) {
+  return a * b;
+}, 12);
+
+console.log(result);
+
+const sum = [1, 2, 3, 4].reduce((a, b) => a * b, 12);
+
+console.log(sum);
