46278ca643c6d576ed0b0334c0b71faa7795f942
thakursachin1004 <sachinst000@gmail.com>
2022-06-19 00:28:10 +0530

    flat without closure

--- JS_Polyfills/array-methods/flat.js
+++ JS_Polyfills/array-methods/flat.js
@@ -1,23 +1,38 @@
-function flat(arr, level) {
-  let flatArr = [];
-  let count = 0;
+// function flat(arr, level) {
+//   let flatArr = [];
+//   let count = 0;
 
-  function child(arr) {
-    arr.map((elt) => {
-      if (typeof elt === "object" && count < level) {
-        count++;
-        child(elt);
-      } else if (typeof elt !== "object") {
-        flatArr.push(elt);
-      } else {
-        flatArr.push(elt);
-        count = 0;
-      }
-    });
-  }
+//   function child(arr) {
+//     arr.map((elt) => {
+//       if (typeof elt === "object" && count < level) {
+//         count++;
+//         child(elt);
+//       } else if (typeof elt !== "object") {
+//         flatArr.push(elt);
+//       } else {
+//         flatArr.push(elt);
+//         count = 0;
+//       }
+//     });
+//   }
 
-  child(arr);
-  console.log(flatArr);
+//   child(arr);
+//   console.log(flatArr);
+// }
+
+// without closures
+
+function flat(arr) {
+  let flattedArr = [];
+  arr.forEach((element) => {
+    if (Array.isArray(element)) {
+      flattedArr = flattedArr.concat(flat(element));
+    } else {
+      flattedArr.push(element);
+    }
+  });
+  return flattedArr;
 }
 
-flat([1, [2, [10, 20, 40, 50], [900, 1000, 2000]], 3, 4], 2);
+const result = flat([1, [2, [10, 20, 40, 50], [900, 1000, 2000]], 3, 4]);
+console.log(result);
