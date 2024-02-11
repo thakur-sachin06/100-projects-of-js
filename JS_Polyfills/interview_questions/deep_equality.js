01e498e3fb678414e9ca649866d72a1cbfe200ff
thakurSachin1004 <sachinst000@gmail.com>
2024-02-11 14:03:40 +0530

    solved deep equality

--- JS_Polyfills/interview_questions/deep_equality.js
+++ JS_Polyfills/interview_questions/deep_equality.js
@@ -1,32 +1,41 @@
-//! deeply compare objects.
-function areEqual(obj1, obj2) {
-  let result = true;
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  if (keys1.length === keys2.length) {
-    keys1.forEach((key) => {
-      if (keys2.indexOf(key) >= 0 && typeof obj1[key] === typeof obj2[key]) {
-        if (typeof obj1[key] === "object") {
-          result = areEqual(obj1[key], obj2[key]);
-        } else if (obj1[key] !== obj2[key]) {
-          result = false;
-        }
-      } else {
-        result = false;
-      }
-    });
-  } else {
-    result = false;
-  }
-  return result;
+// ! The most common application for the toString 'hack' is to figure out what type of object you're dealing with:
+
+// typeof(new Array())  === "object";
+// typeof(new Date())   === "object";
+// typeof(new RegExp()) === "object";
+
+// Object.prototype.toString.call(new Array()).slice(8, -1)  === "Array";
+// Object.prototype.toString.call(new Date()).slice(8, -1)   === "Date";
+// Object.prototype.toString.call(new RegExp()).slice(8, -1) === "RegExp";
+
+function isBothArrOrObj(arg1, arg2) {
+  return (
+    Object.prototype.toString.call(arg1) ===
+    Object.prototype.toString.call(arg2)
+  );
 }
 
-const obj1 = {
-  a: [90, { c: 90 }],
-};
+export default function deepEqual(obj1, obj2) {
+  if (typeof obj1 !== "object" || obj1 === null) {
+    return obj1 === obj2;
+  }
 
-const obj2 = {
-  a: [90, { c: 90 }],
-};
+  if (!isBothArrOrObj(obj1, obj2)) {
+    return false;
+  }
 
-console.log(areEqual(obj1, obj2));
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
